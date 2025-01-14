49b1611023810288f4559b37e72f0c874d3cd714
James Duong (Improving Vancouver Technologies) <v-jamesduong@microsoft.com>
2025-01-14 22:29:22 +0000

    Merged PR 5166: Integrate TLS support
    Integrate TLS support
    
    - Add ITransportSSlHandler trait
    - Implement SslHandler for TDS8
    - Add functionality to capture and restore the original stream.
    - Generalize the design to work for more than TcpStreams
    
    ----
    #### AI description  (iteration 1)
    #### PR Classification
    New feature
    
    #### PR Summary
    This pull request integrates TLS support for secure data transmission using TDS 8.0.
    - `network_transport.rs`: Added TLS handling for network transport, including enabling and disabling SSL.
    - `ssl_handler.rs`: Implemented `Tds8SslHandler` for managing TLS connections.
    - `Cargo.toml`: Added `tokio-native-tls` dependency for TLS support.
    
    Related work items: #33017

--- tds-x/src/connection/transport/network_transport.rs
+++ tds-x/src/connection/transport/network_transport.rs
@@ -1,30 +1,85 @@
 use crate::connection::client_context::ClientContext;
+use crate::connection::transport::ssl_handler::{SslHandler, Tds8SslHandler};
+use crate::core::EncryptionSetting;
+use crate::message::login_options::TdsVersion;
+use async_trait::async_trait;
 use std::io::Error;
-use std::sync::Arc;
-use tokio::io::{AsyncRead, AsyncWrite};
+use tokio::io::{split, AsyncRead, AsyncWrite};
 use tokio::net::TcpStream;
-
-use super::ssl_handler::SslHandler;
 
 pub async fn create_transport(context: &ClientContext) -> Result<Box<NetworkTransport>, Error> {
     let connect_result = TcpStream::connect((context.server_name.as_str(), context.port)).await;
     match connect_result {
         Ok(stream) => {
-            let (reader, writer) = tokio::io::split(stream);
-            Ok(Box::new(NetworkTransport {
-                context,
-                writer: Arc::new(writer),
-                reader: Arc::new(reader),
-            }))
+            // Enable TLS over TCP immediately in TDS 8.0
+            assert!(matches!(context.tds_version(), TdsVersion::V8_0));
+            let ssl_handler = Tds8SslHandler { settings: context };
+
+            // Convert the Tokio stream to a std::TcpStream to make it easier to clone.
+            // Do this outside the callback instead of making the Tokio Stream part of the closure
+            // because into_std() is a destructive operation.
+            let std_stream = stream.into_std()?;
+
+            // Define a cloning callback since this is the only time we know we're working with
+            // TcpStreams.
+            let stream_recoverer = TcpStreamRecoverer {
+                stream: Box::new(std_stream),
+            };
+
+            let encrypted_stream_result = ssl_handler
+                .enable_ssl_async(stream_recoverer.recover_base_stream())
+                .await;
+
+            match encrypted_stream_result {
+                Ok((encrypted_reader, encrypted_writer)) => {
+                    let transport = Box::new(NetworkTransport {
+                        context,
+                        reader: encrypted_reader,
+                        writer: encrypted_writer,
+                        ssl_handler: Box::new(ssl_handler),
+                        stream_recoverer: Box::new(stream_recoverer),
+                    });
+                    Ok(transport)
+                }
+                Err(err) => Err(err),
+            }
         }
         Err(err) => Err(err),
     }
 }
 
+#[async_trait(?Send)]
+pub trait TransportSslHandler {
+    async fn enable_ssl(&mut self) -> Result<(), Error>;
+    async fn disable_ssl(&mut self) -> Result<(), Error>;
+}
+
+pub trait Stream: AsyncRead + AsyncWrite + Unpin {}
+
+impl Stream for TcpStream {}
+
+trait StreamRecoverer {
+    fn recover_base_stream(&self) -> Box<dyn Stream>;
+}
+
+struct TcpStreamRecoverer {
+    pub stream: Box<std::net::TcpStream>,
+}
+
+impl StreamRecoverer for TcpStreamRecoverer {
+    fn recover_base_stream(&self) -> Box<dyn Stream> {
+        let std_stream_clone = self.stream.try_clone().unwrap();
+        let tokio_stream = TcpStream::from_std(std_stream_clone).unwrap();
+        Box::new(tokio_stream)
+    }
+}
+
 pub struct NetworkTransport<'a> {
     context: &'a ClientContext,
-    writer: Arc<dyn AsyncWrite>,
-    reader: Arc<dyn AsyncRead>,
+    reader: Box<dyn AsyncRead + 'a>,
+    writer: Box<dyn AsyncWrite + 'a>,
+    ssl_handler: Box<dyn SslHandler + 'a>,
+    stream_recoverer: Box<dyn StreamRecoverer + 'a>,
 }
 
 impl NetworkTransport<'_> {
@@ -32,16 +87,61 @@ impl NetworkTransport<'_> {
     async fn receive(&self, _data: &[u8]) -> i64 {
         0
     }
-    fn enable_buffered_reader(&self) {}
 
-    fn ssl_handler(&self) -> Box<dyn SslHandler> {
-        struct DummySsl {}
-
-        impl SslHandler for DummySsl {
-            fn enable(&self) {}
-            fn disable(&self) {}
+    async fn enable_ssl_internal(&mut self) -> Result<(), Error> {
+        match self
+            .ssl_handler
+            .enable_ssl_async(self.stream_recoverer.recover_base_stream())
+            .await
+        {
+            Ok((encrypted_reader, encrypted_writer)) => {
+                self.reader = encrypted_reader;
+                self.writer = encrypted_writer;
+                Ok(())
+            }
+            Err(err) => Err(err),
         }
+    }
 
-        Box::new(DummySsl {})
+    async fn disable_ssl_internal(&mut self) {
+        // Notify the SSL handler that SSL is being disabled.
+        self.ssl_handler.shutdown_ssl();
+
+        // Update the reader and writer. Note - buffering should get re-enabled on the reader
+        // if it was applied previously here.
+        let base_stream = self.stream_recoverer.recover_base_stream();
+        let (base_reader, base_writer) = split(base_stream);
+        self.reader = Box::new(base_reader);
+        self.writer = Box::new(base_writer)
+    }
+}
+
+#[async_trait(?Send)]
+impl TransportSslHandler for NetworkTransport<'_> {
+    async fn enable_ssl(&mut self) -> Result<(), Error> {
+        self.enable_ssl_internal().await
+    }
+
+    async fn disable_ssl(&mut self) -> Result<(), Error> {
+        let encryption_type_check = match self.context.encryption {
+            EncryptionSetting::NotSupported => Ok(()),
+            EncryptionSetting::Optional => Ok(()),
+            EncryptionSetting::Required => Ok(()),
+            EncryptionSetting::Strict => {
+                // TODO: Evaluate this error.
+                Err(Error::new(
+                    std::io::ErrorKind::InvalidInput,
+                    "Under strict mode the client must communicate over TLS",
+                ))
+            }
+            EncryptionSetting::LoginOnly => Ok(()),
+        };
+
+        if encryption_type_check.is_err() {
+            encryption_type_check
+        } else {
+            self.disable_ssl_internal().await;
+            Ok(())
+        }
     }
 }
--- tds-x/src/connection/transport/ssl_handler.rs
+++ tds-x/src/connection/transport/ssl_handler.rs
@@ -1,4 +1,50 @@
+use crate::connection::client_context::ClientContext;
+use crate::connection::transport::network_transport::Stream;
+use async_trait::async_trait;
+use std::io::Error;
+use tokio::io::{AsyncRead, AsyncWrite};
+use tokio_native_tls::native_tls::TlsConnector;
+
+#[async_trait(?Send)]
 pub trait SslHandler {
-    fn enable(&self);
-    fn disable(&self);
+    async fn enable_ssl_async(
+        &self,
+        base_stream: Box<dyn Stream>,
+    ) -> Result<(Box<dyn AsyncRead>, Box<dyn AsyncWrite>), Error>;
+    fn shutdown_ssl(&self);
+}
+
+pub struct Tds8SslHandler<'a> {
+    pub settings: &'a ClientContext,
+}
+
+#[async_trait(?Send)]
+impl<'a> SslHandler for Tds8SslHandler<'a> {
+    async fn enable_ssl_async(
+        &self,
+        base_stream: Box<dyn Stream>,
+    ) -> Result<(Box<dyn AsyncRead>, Box<dyn AsyncWrite>), Error> {
+        // Build the native TlsConnector directly because tokio-native-tls's version
+        // is missing some functionality.
+        let connector = TlsConnector::builder()
+            .danger_accept_invalid_certs(true)
+            .build()
+            .unwrap();
+
+        let encrypted_stream = tokio_native_tls::TlsConnector::from(connector)
+            .connect(self.settings.server_name.as_str(), base_stream)
+            .await;
+
+        match encrypted_stream {
+            Ok(stream) => {
+                let (read_half, write_half) = tokio::io::split(stream);
+                Ok((Box::new(read_half), Box::new(write_half)))
+            }
+            Err(e) => Err(std::io::Error::new(std::io::ErrorKind::Other, e)),
+        }
+    }
+
+    fn shutdown_ssl(&self) {
+        panic!("Cannot disable TLS for TDS version 8.0")
+    }
 }
