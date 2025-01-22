909cc3293820c83b86ef7f60350f68d9d931ef7b
Saurabh Singh <saurabh.singh@microsoft.com>
2025-01-22 20:41:12 +0000

    Merged PR 5204: Enhancing OptionFlags for persistence
    Adding value computation for OptionFlags and the related tests.
    
    ----
    #### AI description  (iteration 1)
    #### PR Classification
    Enhancement of existing feature.
    
    #### PR Summary
    This pull request enhances the `OptionFlags` for better persistence and includes additional tests.
    - `src/message/login_options.rs`: Implemented bit-setting functions for `OptionFlags2` and `OptionFlags3`, added tests for default and custom values, and refactored bit-setting logic.
    - Added `PartialEq` derive for several enums to facilitate comparisons in tests.
    
    Related work items: #33427

--- tds-x/src/message/login_options.rs
+++ tds-x/src/message/login_options.rs
@@ -122,16 +122,8 @@ impl OptionFlags1 {
         }
     }
 
-    fn set_bit(value: &mut u8, condition: bool, bit_index: u8) {
-        if condition {
-            *value |= bit_index;
-        } else {
-            *value &= u8::MAX - bit_index;
-        }
-    }
-
     fn set_endian_bit(&self, value: &mut u8) {
-        Self::set_bit(
+        set_options_bit(
             value,
             self.endian != OptionEndian::LittleEndian,
             Self::OPTION_ENDIAN_BIT_INDEX,
@@ -139,7 +131,7 @@ impl OptionFlags1 {
     }
 
     fn set_charset_bit(&self, value: &mut u8) {
-        Self::set_bit(
+        set_options_bit(
             value,
             self.charset != OptionCharset::Ascii,
             Self::OPTION_CHARSET_BIT_INDEX,
@@ -164,7 +156,7 @@ impl OptionFlags1 {
     }
 
     fn set_bcp_dumpload_bit(&self, value: &mut u8) {
-        Self::set_bit(
+        set_options_bit(
             value,
             self.bcp_dumpload != OptionBcpDumpload::On,
             Self::OPTION_BCP_DUMPLOAD_BIT_INDEX,
@@ -172,7 +164,7 @@ impl OptionFlags1 {
     }
 
     fn set_use_db_bit(&self, value: &mut u8) {
-        Self::set_bit(
+        set_options_bit(
             value,
             self.use_db == OptionUseDb::On,
             Self::OPTION_USE_DB_BIT_INDEX,
@@ -180,7 +172,7 @@ impl OptionFlags1 {
     }
 
     fn set_init_db_bit(&self, value: &mut u8) {
-        Self::set_bit(
+        set_options_bit(
             value,
             self.init_db == OptionInitDb::Fatal,
             Self::OPTION_INIT_DB_INDEX_BIT,
@@ -188,7 +180,7 @@ impl OptionFlags1 {
     }
 
     fn set_lang_warn_bit(&self, value: &mut u8) {
-        Self::set_bit(
+        set_options_bit(
             value,
             self.lang_warn == OptionLangWarn::On,
             Self::OPTION_LANG_WARN_BIT_INDEX,
@@ -211,16 +203,19 @@ impl OptionsValue for OptionFlags1 {
     }
 }
 
+#[derive(PartialEq)]
 pub enum OptionInitLang {
     Warn,
     Fatal,
 }
 
+#[derive(PartialEq)]
 pub enum OptionOdbc {
     Off,
     On,
 }
 
+#[derive(PartialEq)]
 pub enum OptionUser {
     Normal,
     Reserved,
@@ -228,6 +223,7 @@ pub enum OptionUser {
     ReplicationLogin,
 }
 
+#[derive(PartialEq)]
 pub enum OptionIntegratedSecurity {
     Off,
     On,
@@ -240,12 +236,72 @@ pub struct OptionFlags2 {
     pub(crate) integrated_security: OptionIntegratedSecurity,
 }
 
-impl OptionsValue for OptionFlags2 {
-    fn value(&self) -> u8 {
-        todo!()
+impl OptionFlags2 {
+    const OPTION_INIT_LANG_BIT_INDEX: u8 = 0x01;
+    const OPTION_ODBC_BIT_INDEX: u8 = 0x02;
+    const OPTION_USER_BIT_INDEX_SERVER: u8 = 0x10;
+    const OPTION_USER_BIT_INDEX_REM_USER: u8 = 0x20;
+    const OPTION_USER_BIT_INDEX_SQL_REPL: u8 = 0x40;
+    const OPTION_INTEGRATED_SECURITY_BIT_INDEX: u8 = 0x80;
+
+    fn set_init_lang_bit(&self, value: &mut u8) {
+        set_options_bit(
+            value,
+            self.init_lang == OptionInitLang::Fatal,
+            Self::OPTION_INIT_LANG_BIT_INDEX,
+        );
+    }
+
+    fn set_odbc_bit(&self, value: &mut u8) {
+        set_options_bit(
+            value,
+            self.odbc == OptionOdbc::On,
+            Self::OPTION_ODBC_BIT_INDEX,
+        );
+    }
+
+    fn set_user_bit(&self, value: &mut u8) {
+        if self.user == OptionUser::Normal {
+            *value &= u8::MAX - Self::OPTION_USER_BIT_INDEX_SERVER;
+            *value &= u8::MAX - Self::OPTION_USER_BIT_INDEX_REM_USER;
+            *value &= u8::MAX - Self::OPTION_USER_BIT_INDEX_SQL_REPL;
+        } else if self.user == OptionUser::Reserved {
+            *value |= Self::OPTION_USER_BIT_INDEX_SERVER;
+            *value &= u8::MAX - Self::OPTION_USER_BIT_INDEX_REM_USER;
+            *value &= u8::MAX - Self::OPTION_USER_BIT_INDEX_SQL_REPL;
+        } else if self.user == OptionUser::RemUser {
+            *value &= u8::MAX - Self::OPTION_USER_BIT_INDEX_SERVER;
+            *value |= Self::OPTION_USER_BIT_INDEX_REM_USER;
+            *value &= u8::MAX - Self::OPTION_USER_BIT_INDEX_SQL_REPL;
+        } else if self.user == OptionUser::ReplicationLogin {
+            *value &= u8::MAX - Self::OPTION_USER_BIT_INDEX_SERVER;
+            *value &= u8::MAX - Self::OPTION_USER_BIT_INDEX_REM_USER;
+            *value |= Self::OPTION_USER_BIT_INDEX_SQL_REPL;
+        }
+    }
+
+    fn set_integrated_security_bit(&self, value: &mut u8) {
+        set_options_bit(
+            value,
+            self.integrated_security == OptionIntegratedSecurity::On,
+            Self::OPTION_INTEGRATED_SECURITY_BIT_INDEX,
+        );
     }
 }
 
+impl OptionsValue for OptionFlags2 {
+    fn value(&self) -> u8 {
+        let mut computed_value: u8 = 0;
+
+        self.set_init_lang_bit(&mut computed_value);
+        self.set_integrated_security_bit(&mut computed_value);
+        self.set_odbc_bit(&mut computed_value);
+        self.set_user_bit(&mut computed_value);
+        computed_value
+    }
+}
+
+#[derive(PartialEq)]
 pub enum OptionChangePassword {
     No,
     Yes,
@@ -259,8 +315,136 @@ pub struct OptionFlags3 {
     pub(crate) unknown_collation_handling: bool,
 }
 
+impl OptionFlags3 {
+    const OPTION_CHANGE_PASSWORD_BIT_INDEX: u8 = 0x01;
+    const OPTION_BINARY_XML_BIT_INDEX: u8 = 0x02;
+    const OPTION_SPAWN_USER_INSTANCE_BIT_INDEX: u8 = 0x04;
+    const OPTION_UNKNOWN_COLLATION_HANDLING_BIT_INDEX: u8 = 0x08;
+    const OPTION_EXTENSION_USED_BIT_INDEX: u8 = 0x10;
+
+    fn set_change_password_bit(&self, value: &mut u8) {
+        set_options_bit(
+            value,
+            self.change_password == OptionChangePassword::Yes,
+            Self::OPTION_CHANGE_PASSWORD_BIT_INDEX,
+        );
+    }
+
+    fn set_binary_xml_bit(&self, value: &mut u8) {
+        set_options_bit(value, self.binary_xml, Self::OPTION_BINARY_XML_BIT_INDEX);
+    }
+
+    fn set_spawn_user_instance_bit(&self, value: &mut u8) {
+        set_options_bit(
+            value,
+            self.spawn_user_instance,
+            Self::OPTION_SPAWN_USER_INSTANCE_BIT_INDEX,
+        );
+    }
+
+    fn set_extension_used_bit(&self, value: &mut u8) {
+        set_options_bit(
+            value,
+            self.extension_used,
+            Self::OPTION_EXTENSION_USED_BIT_INDEX,
+        );
+    }
+
+    fn set_unknown_collation_handling_bit(&self, value: &mut u8) {
+        set_options_bit(
+            value,
+            self.unknown_collation_handling,
+            Self::OPTION_UNKNOWN_COLLATION_HANDLING_BIT_INDEX,
+        );
+    }
+}
+
 impl OptionsValue for OptionFlags3 {
     fn value(&self) -> u8 {
-        todo!()
+        let mut computed_value: u8 = 0;
+
+        self.set_change_password_bit(&mut computed_value);
+        self.set_binary_xml_bit(&mut computed_value);
+        self.set_spawn_user_instance_bit(&mut computed_value);
+        self.set_extension_used_bit(&mut computed_value);
+        self.set_unknown_collation_handling_bit(&mut computed_value);
+        computed_value
+    }
+}
+
+fn set_options_bit(value: &mut u8, condition_or_bit_index: bool, bit_index: u8) {
+    if condition_or_bit_index {
+        *value |= bit_index;
+    } else {
+        *value &= u8::MAX - bit_index;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_option_flags1_default_value() {
+        let flags = OptionFlags1::default();
+        assert_eq!(flags.value(), 0xF3);
+    }
+
+    #[test]
+    fn test_option_flags1_custom_value() {
+        let flags = OptionFlags1 {
+            endian: OptionEndian::LittleEndian,
+            charset: OptionCharset::Ascii,
+            float: OptionFloat::IEEE,
+            bcp_dumpload: OptionBcpDumpload::On,
+            use_db: OptionUseDb::On,
+            init_db: OptionInitDb::Fatal,
+            lang_warn: OptionLangWarn::On,
+        };
+        assert_eq!(flags.value(), 224);
+    }
+
+    #[test]
+    fn test_option_flags2_default_value() {
+        let flags2 = OptionFlags2 {
+            init_lang: OptionInitLang::Fatal, // change to language needs to succed for succesful login
+            odbc: OptionOdbc::On,
+            user: OptionUser::Normal,
+            integrated_security: OptionIntegratedSecurity::Off,
+        };
+        // Assert.Equal(3, flags2.Value);
+        assert_eq!(flags2.value(), 3);
+
+        let flags2_1 = OptionFlags2 {
+            init_lang: OptionInitLang::Fatal, // change to language needs to succed for succesful login
+            odbc: OptionOdbc::On,
+            user: OptionUser::ReplicationLogin,
+            integrated_security: OptionIntegratedSecurity::Off,
+        };
+        assert_eq!(flags2_1.value(), 67);
+    }
+
+    #[test]
+    fn test_option_flags3_default_value() {
+        let flags = OptionFlags3 {
+            change_password: OptionChangePassword::No,
+            binary_xml: false,
+            spawn_user_instance: false,
+            extension_used: false,
+            unknown_collation_handling: false,
+        };
+        assert_eq!(flags.value(), 0x00);
+    }
+
+    #[test]
+    fn test_option_flags3_custom_value() {
+        let flags = OptionFlags3 {
+            change_password: OptionChangePassword::No,
+            binary_xml: false,
+            spawn_user_instance: false,
+            extension_used: true,
+            unknown_collation_handling: false,
+        };
+        assert_eq!(flags.value(), 16);
     }
 }
